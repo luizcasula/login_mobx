6454edbe79a4941563423225385420bfa68e201e
Luiz Casula <luizcasulaf@gmail.com>
2020-04-22 23:45:20 -0400

    create results

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -3,15 +3,40 @@ import 'package:login_mobx/stores/app_store.dart';
 import 'package:provider/provider.dart';
 
 class HomeView extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     AppStore store = Provider.of<AppStore>(context);
+
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Text(store.name),
+        child: Container(
+          padding: EdgeInsets.only(top: 32),
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: NetworkImage(
+                  store.picture,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 32),
+                child: Text(
+                  store.name,
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: Text(
+                  store.email,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
